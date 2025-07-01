55431144573db6385b641633ffe418d4075918ac
abrehm <alexander.brehm@e-hps.com>
2025-07-01 15:34:24 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# publish test
+
+Seems to work over a UNC share when using .net sdk 8.0.110, but not 8.0.111
---
+++ publish-test/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+try
+{
+  Console.WriteLine("Hello, World!");
+}
+catch (Exception e)
+{
+  Console.WriteLine($"Exception: {e.Message}");
+}
